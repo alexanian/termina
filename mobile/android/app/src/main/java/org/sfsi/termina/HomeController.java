8d866b9ee5d42a6c20e85fff4814cb503d5089fa
Alex Korzec <alex.korzec@smule.com>
2017-03-04 15:27:13 -0800

    Add Conductor and skeleton for a Controller.

---
+++ mobile/android/app/src/main/java/org/sfsi/termina/HomeController.java
@@ -0,0 +1,19 @@
+package org.sfsi.termina;
+
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bluelinelabs.conductor.Controller;
+
+/**
+ * Created by Smule on 3/4/17.
+ */
+public class HomeController extends Controller {
+    @NonNull
+    @Override
+    protected View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container) {
+        return null;
+    }
+}
--- mobile/android/app/src/main/java/org/sfsi/termina/MasterActivity.java
+++ mobile/android/app/src/main/java/org/sfsi/termina/MasterActivity.java
@@ -2,14 +2,34 @@ package org.sfsi.termina;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import com.bluelinelabs.conductor.Conductor;
+import com.bluelinelabs.conductor.Router;
+import com.bluelinelabs.conductor.RouterTransaction;
+
 import org.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.activity_master)
 public class MasterActivity extends AppCompatActivity {
+    private Router router;
 
-    //hello
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ViewGroup container = (ViewGroup)findViewById(R.id.controller_container);
+
+        router = Conductor.attachRouter(this, container, savedInstanceState);
+        if (!router.hasRootController()) {
+            router.setRoot(RouterTransaction.with(new HomeController()));
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!router.handleBack()) {
+            super.onBackPressed();
+        }
     }
 }
