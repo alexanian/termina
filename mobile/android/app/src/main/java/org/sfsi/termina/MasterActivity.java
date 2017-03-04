53643cccbfd6ae3542c4a739297bc04b34e0cda5
Sean Oh <sean.oh@smule.com>
2017-03-04 11:55:32 -0800

    android project init

---
+++ mobile/android/app/src/main/java/org/sfsi/termina/MasterActivity.java
@@ -0,0 +1,14 @@
+package org.sfsi.termina;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_master)
+public class MasterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
