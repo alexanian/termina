14cea01a3cee90c2dcbba11a3aa2f789db6103bc
Philip Stehlik <p@pstehlik.com>
2017-03-04 15:31:24 -0800

    pre-move procedure option into module

---
+++ sheet2json/index_ps.js
@@ -0,0 +1,7 @@
+var sheetprocessing = require('./sheetprocessing');
+var globalData = require('./globaldata');
+
+
+sheetprocessing.pullProcedureOptions(
+    
+);
--- sheet2json/sheetprocessing.js
+++ sheet2json/sheetprocessing.js
@@ -35,6 +35,46 @@ function _pullProcedureOptions(successCallback) {
     _pullSingleTabFromSheet(successCallback, config.gapi.tabs.procedureOptions)
 }
 
+function _getAndStoreProcedureOptions() {
+    _pullSingleTabFromSheet((response) => {
+          console.log("--- Pulled Procedure Option Data ---");
+          console.log("Pulled this many procedure options:" + response.values.length);
+          
+          var optionsList = [];
+          var rows = response.values;
+          
+          var createOptionsObject = function(optionId, type, min_days_lmp, max_days_lmp, state){
+              var cleanedState = state.replace(/\s/g,'');
+              var singleOption = {
+                  optionId: optionId,
+                  type: type,
+                  min_days_lmp: min_days_lmp,
+                  max_days_lmp: max_days_lmp,
+                  state: cleanedState.split(',')
+              };
+              return singleOption;
+          };
+          // Structure in the google sheet is
+          // type | Min preg length (days LMP) | Max preg length (days LMP) | state
+          for (var i = 0; i < rows.length; i++) {
+              var row = rows[i];
+              optionsList.push(
+                  createOptionsObject(
+                      i,
+                      row[0],
+                      row[1],
+                      row[2],
+                      row[3]
+                  )
+              );
+          }
+          globalData.options = optionsList;
+          console.log(globalData.options);
+      }, 
+      config.gapi.tabs.procedureOptions
+    )
+}
+
 function _pullAgeWarning(successCallback) {
     _pullSingleTabFromSheet(successCallback, config.gapi.tabs.ageWarning)
 }
