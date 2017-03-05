5eb294214a0fbe1b24aee7dffd0625c46cb78c45
Sierra Brown <sbrown@23andme.com>
2017-03-04 17:27:10 -0800

    remove logs

--- sheet2json/server.js
+++ sheet2json/server.js
@@ -16,13 +16,11 @@ app.get('/options', function(req, res) {
 
 	// Parse Query
 	var baseOptions = globalData.options;
-	console.log('global data');
-	console.log(baseOptions)
 	var url_parts = url.parse(req.url, true);
 	var query = url_parts.query;
 	var userAge = parseInt(query.age);
 	var userState = query.state;
-	var userDaysSince = parseInt(query.days_since)
+	var userDaysSince = parseInt(query.days_since);
 
 	// Array of options to return to the user
 	var optionsToReturn = bizLogic.filterAvailableOptions(baseOptions, userState, userDaysSince);
